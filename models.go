968c0b66eff703dc73d7feaff6fc537cf8646e69
rubnsbarbosa <rubensbarbosa90@gmail.com>
2025-03-15 16:40:40 +0100

    uf and equipe struct added

--- models.go
+++ models.go
@@ -7,15 +7,36 @@ type Premio struct {
 	ValorPremio    float64 `json:"valorPremio"`
 }
 
+type UF struct {
+	Municipio    string `json:"municipio"`
+	NomeFatansia string `json:"nomeFatansiaUL"`
+	Posicao      int    `json:"posicao"`
+	UF           string `json:"uf"`
+}
+
+type Equipe struct {
+	DiaJogo      string `json:"diaSemana"`
+	DataJogo     string `json:"dtJogo"`
+	EquipeUm     string `json:"nomeEquipeUm"`
+	EquipeDois   string `json:"nomeEquipeDois"`
+	NumGolEqUm   int    `json:"nuGolEquipeUm"`
+	NumGolEqDois int    `json:"nuGolEquipeDois"`
+}
+
 type Data struct {
 	Acumulado    bool     `json:"acumulado"`
 	DataApuracao string   `json:"dataApuracao"`
 	DataProximoS string   `json:"dataProximoConcurso"`
 	NumSorteados []string `json:"listaDezenas"`
+	ND2Sorteados []string `json:"listaDezenasSegundoSorteio"`
 	TrvSorteados []string `json:"trevosSorteados"`
 	ListaPremios []Premio `json:"listaRateioPremio"`
+	UFGanhadores []UF     `json:"listaMunicipioUFGanhadores"`
+	EqEsportivas []Equipe `json:"listaResultadoEquipeEsportiva"`
 	LocalSorteio string   `json:"localSorteio"`
 	MunicSorteio string   `json:"nomeMunicipioUFSorteio"`
 	ConcursoNume int      `json:"numero"`
 	ProximoValor float64  `json:"valorEstimadoProximoConcurso"`
+	ValAcumulado float64  `json:"valorAcumuladoProximoConcurso"`
+	ValAcumula05 float64  `json:"valorAcumuladoConcurso_0_5"`
 }
